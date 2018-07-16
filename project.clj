d8ac1557aa839f46d38e8c634879d6275ccf2028
Michael Schwager <mschwager@kymetacorp.com>
2018-07-15 23:27:49 -0700

    Removed modules from .gitmodules; added Java files and resources directly so no longer dependent on DockFX project

--- project.clj
+++ project.clj
@@ -3,10 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.dockfx/dockfx "0.1.7-48-gbbbc576"]
-                 [jfxutils "0.1.0-SNAPSHOT"]
+  :dependencies [;;[org.dockfx/dockfx "0.1.7-48-gbbbc576"]
+                 ;;[jfxutils "0.1.0-SNAPSHOT"]
 				 ]
-
+ 
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"]
   :main docks.demo
   :aot :all
   )
